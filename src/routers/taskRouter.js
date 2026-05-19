b3de2d3a059a5a1cdda77992dd9911b0a92046e2
Sudan Basnet <83904391+SudanBasnet@users.noreply.github.com>
2026-05-19 20:42:47 +1000

    delete: filter used for delete

--- src/routers/taskRouter.js
+++ src/routers/taskRouter.js
@@ -10,34 +10,58 @@ const router = express.Router();
 //   next();
 // });
 
-let fakeDB = [];
+let fakeDB = [
+  { id: 1, task: "coding", hr: 20, type: "entry" },
+  { id: 2, task: "gaming", hr: 4, type: "entry" },
+  { id: 3, task: "sleeping", hr: 7, type: "entry" },
+];
+
+//!Create item
 router.post("/", (req, res, next) => {
   //do your code
   fakeDB.push(req.body);
   console.log(fakeDB);
-  console.log(req.body);
+  // console.log(req.body);
   res.json({
     message: "success",
-    status: "response from post",
+    status: "New Task has been added Successfully",
   });
 });
+
 router.get("/", (req, res, next) => {
   //do your code
   res.json({
     message: "success",
     status: "response from get",
+    tasks: fakeDB,
   });
 });
 
-router.put("/", (req, res, next) => {
+router.patch("/", (req, res, next) => {
+  console.log(req.body);
   //do your code
+  const { id, type } = req.body;
+  fakeDB = fakeDB.map((item) => {
+    if (item.id === id) {
+      item.type = type;
+      return item;
+    } else {
+      return item;
+    }
+  });
   res.json({
     message: "success",
-    status: "response from put",
+    status: "response from put: your task has been updated successfully",
   });
 });
-router.delete("/", (req, res, next) => {
+
+//!delete
+router.delete("/:id", (req, res, next) => {
   //do your code
+  const { id } = req.params;
+  console.log(typeof id);
+
+  fakeDB = fakeDB.filter((item) => item.id !== +id);
   res.json({
     message: "success",
     status: "response from delete",
